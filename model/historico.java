513f17f8d57c5701f6e1b5f601fa5889d3458af7
William Santos <165169533+WilliamCutelo@users.noreply.github.com>
2025-04-15 15:48:25 -0300

    historico iniciado, enviado por upload

---
+++ model/historico.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class historico {
+    private List<transacao> transacoes;
+
+    public historico() {
+        this.transacoes = new ArrayList<>();
+    }
+
+    public void adicionarTransacao(transacao t){
+        transacoes.add(t);
+    }
+
+    public List<transacao> getTransacaos() {
+        return transacoes;
+    }
+
+    public void exibirHistorico () {
+        System.out.println("Historico de transações:");
+        for (transacao t : transacoes) {
+            System.out.println("Tipo: " + t.getTipo() + ", Descrição: " + t.getDescricao() + ", Valor: " + t.getValor() + ", Data: " + t.getData());
+        }
+    }
+}
