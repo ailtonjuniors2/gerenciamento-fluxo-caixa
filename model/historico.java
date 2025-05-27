fab863f73498c54e323e6a5428263b21caaa624d
William Santos <165169533+WilliamCutelo@users.noreply.github.com>
2025-05-27 00:12:21 -0300

    adicionado pop

--- model/historico.java
+++ model/historico.java
@@ -12,11 +12,11 @@ public class historico { //criação do historico
         this.transacoes = new ArrayList<>();
     }
 
-    public void adicionarTransacao(transacao t) {
+    public void adicionarTransacao(transacao t) { //adiciona transação
         transacoes.push(t);
     }
 
-    public List<transacao> getTransacoes() {
+    public Stack<transacao> getTransacoes() { // pega a transação
         return transacoes;
     }
 
@@ -26,4 +26,13 @@ public class historico { //criação do historico
             System.out.println("Tipo: " + t.getTipo() + ", Descrição: " + t.getDescricao() + ", Valor: " + t.getValor() + ", Data: " + t.getData());
         }
     }
-}
+
+    public void removerTransacao() {
+        if (!transacoes.isEmpty()) {
+            transacao removida = transacoes.pop();
+            System.out.println("Transação desfeita: " + removida.getDescricao());
+                } else {
+                    System.out.println("Nenhuma transação para desfazer.");
+                }
+            }
+        }
